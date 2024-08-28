97e977ae5f9c8cc1f2532d505b2304e23345717f
YuiHironaka <81809@DESKTOP-I27R6NU>
2024-08-28 20:50:07 +0900

    Review04の作成

---
+++ src/animal/Human.java
@@ -0,0 +1,16 @@
+package animal;
+
+
+ class Human extends Animal implements Thinkable {
+     private String hobby;
+    
+    public Human(String name, int age, String hobby) {
+        super(name,age);
+        this.hobby = hobby;
+    }
+
+     public void think () {
+     System.out.println("私は" + hobby + "について考えています。");
+     }
+     
+}
